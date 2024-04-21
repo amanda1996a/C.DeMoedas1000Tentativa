9561dc4b0dbfbfb628486208a387581893289edd
amanda1996a <amanda.ss79@hotmail.com>
2024-04-21 18:54:05 -0300

    Meu conversor de moedas

---
+++ scripts.js
@@ -0,0 +1,84 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real
+    const currencyValueConverted = document.querySelector(".currency-value") // Outras moedas
+
+
+    const dolarToday = 5.2  // Extrai as taxas de câmbio do objeto de dados
+    const euroToday = 5.5
+    const libraToday = 6.4
+    const bitcoinToday = 336.9
+
+
+    if (currencySelect.value == "dolar") { // Se o select estiver selecionado o valor de dolar, entre aqui 
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+
+    if (currencySelect.value == "euro") { // Se o select estiver selecionado o valor de euro, enre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    if (currencySelect.value == "libra") { // Se o select estiver selecionado o valor de euro, enre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+
+     // Converte o valor de entrada com base na moeda de destino selecionada
+    if (currencySelect.value == "bitcoin") { // Se o select estiver selecionado o valor de euro, enre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputCurrencyValue / bitcoinToday)
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { // Atualiza o valor convertido e o valor de entrada exibido na página
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+}
+
+function changeCurrency() { // Atualiza o nome da moeda e a imagem com base na moeda de destino selecionada
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assets/dolar.png"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.jpeg"
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/libra.png"
+    }
+
+    if (currencySelect.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./assets/bitcoin.png"
+    }
+
+    convertValues() // Chama a função de conversão para atualizar os valores na página
+}
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
+// Adiciona listeners para os eventos de mudança de moeda e clique no botão de conversão
